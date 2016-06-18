104e30ebc866624ebbce12ea8451c859474c0ef0
codeapps <codeapps@aol.com>
2016-06-17 23:09:40 -0500

    Add seed for Database

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,38 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('manufacturers')->insert([
+            'name' => "Apple",
+        ]);
+
+        DB::table('categories')->insert([
+            'name' => "Computers",
+        ]);
+
+        DB::table('devices')->insert([
+            'name' => "Macbook",
+        ]);
+
+        DB::table('maintenances')->insert([
+            'name' => "Install Windows 10",
+        ]);
+
+        DB::table('maintenances')->insert([
+            'name' => "Install Atom",
+        ]);
+
+        DB::table('users')->insert([
+            'username' => "codeapps",
+        ]);
+
+        DB::table('states')->insert([
+            'name' => "Good",
+        ]);
+
+        $main1 = App\Maintenance::find(1);
+        $main2 = App\Maintenance::find(2);
+        $device = App\Device::find(1);
+        $category = App\Category::find(1);
+        $manufacturer = App\Manufacturer::find(1);
     }
 }
