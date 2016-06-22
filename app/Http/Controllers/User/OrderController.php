16d8a9f0c75a504dc4b9447b38b07f70f3071dd2
codeapps <codeapps@aol.com>
2016-06-22 01:24:06 -0500

    Routes

---
+++ app/Http/Controllers/User/OrderController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers\User;
+
+use App\Order;
+use Illuminate\Http\Request;
+use App\Http\Requests;
+
+class OrderController extends Controller
+{
+    //
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,30 +11,16 @@
 |
 */
 
-Route::group(['namespace' => 'Admin'], function()
-{
-    // Controllers Within The "App\Http\Controllers\Admin" Namespace
-});
+Route::get('/users', 'Users@read');
 
-Route::group(['namespace' => 'User'], function()
-{
-  Route::get('/users', 'Users@read');
+Route::get('/users/{user}', 'Users@profile');
+Route::patch('/users/{user}', 'Users@update');
 
-  Route::get('/users/{user}', 'Users@profile');
-  Route::patch('/users/{user}', 'Users@update');
+Route::get('/users/delete', 'Users@delete');
 
-  Route::get('/users/delete', 'Users@delete');
+Route::get('/', 'HomeController@index');
+Route::get('/dashboard', 'HomeController@welcome');
+Route::get('/about', 'HomeController@about');
+Route::get('/contact', 'HomeController@contact');
 
-  Route::get('/', 'HomeController@index');
-  Route::get('/dashboard', 'HomeController@welcome');
-  Route::get('/about', 'HomeController@about');
-  Route::get('/contact', 'HomeController@contact');
-
-  Route::auth();
-
-});
-
-Route::group(['namespace' => 'Owner'], function()
-{
-    // Controllers Within The "App\Http\Controllers\Admin" Namespace
-});
+Route::auth();
