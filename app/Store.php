53e38d9014190762c5a2eed9f2bd2b748445e369
codeapps <codeapps@aol.com>
2016-07-18 01:54:36 -0500

    Add Store Model

--- app/Store.php
+++ app/Store.php
@@ -11,9 +11,24 @@ class Store extends Model
         return $this->hasMany(Product::class);
     }
 
+    public function region()
+    {
+        return $this->belongsTo(Region::class);
+    }
+
+    public function region_id()
+    {
+        return $this->region->id;
+    }
+
     public function city()
     {
         return $this->belongsTo(City::class);
+    }
+
+    public function city_id()
+    {
+        return $this->city->id;
     }
 
     public function users()
