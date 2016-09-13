9bdf613bb256536ede57b845dfdfa01a74dac408
AdmonONE <jjcabezas8@misena.edu.co>
2016-09-13 01:01:30 -0500

    AdmonOne - Translate

---
+++ resources/lang/es/strings.php
@@ -0,0 +1,22 @@
+<?php
+
+return array(
+
+
+	'E-Mail' => 'Correo Electrónico',
+	'Password' => 'Contraseña',
+	'Rememberme' => 'Recordarme',
+	'Forgot' => 'Olvidó su contraseña?',
+	'Username' => 'Nombre de Usuario',
+	'DNI' => 'Identificación',
+	'ConfirPass' => 'Confirmar Contraseña',
+	'Register' => 'Registrar',
+	'ResetPass' => 'Restablecer Contraseña',
+	'SendResetPass' => 'Enviar Link de Restablecimiento',
+	'Login' => 'Iniciar Sesión'
+	
+	
+	
+
+	);
+?>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -61,8 +61,8 @@
         <ul class="nav navbar-nav navbar-right">
           <!-- Authentication Links -->
           @if (Auth::guest())
-            <li><a href="{{ url('/login') }}">Login</a></li>
-            <li><a href="{{ url('/register') }}">Register</a></li>
+            <li><a href="{{ url('/login') }}">{{trans('strings.Login')}}</a></li>
+            <li><a href="{{ url('/register') }}">{{trans('strings.Register')}}</a></li>
 
           @else
             @if(Auth::user()->role_id == 1 || Auth::user()->role_id == 4)
