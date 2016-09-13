26bdffc6fe72a9b919eb9f8e1e4214b30ca4f693
codeapps <codeapps@aol.com>
2016-09-13 15:18:22 -0500

    Complete Static Public Files

--- resources/views/layouts/links.blade.php
+++ resources/views/layouts/links.blade.php
@@ -1,10 +1,9 @@
 <!-- Fonts -->
-<link rel="stylesheet" type="text/css" href="{{ asset('css/font-awesome.min.css') }}">
+<link rel="stylesheet" type="text/css" href="/css/font-awesome-4.6.3/css/font-awesome.min.css">
 
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700">
 
 <!-- Styles -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-{{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+<link rel="stylesheet" href="/css/bootstrap-3.3.7/css/bootstrap.min.css">
 
 <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}">
