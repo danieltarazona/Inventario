51eab4852cd4b850e3577cb456e5ac9ae61577e0
codeapps <codeapps@aol.com>
2016-10-12 10:48:33 -0500

    Dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,6 +5,21 @@ namespace App\Http\Controllers;
 use App\Http\Requests;
 use Illuminate\Http\Request;
 
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Input;
+
+use App\Dashboard;
+use App\Cart;
+use App\Sale;
+use App\Store;
+use App\Order;
+use App\User;
+use App\City;
+use App\Issue;
+use App\Maintenance;
+use App\Product;
+use App\Region;
+
 class HomeController extends Controller
 {
     /**
@@ -24,7 +39,20 @@ class HomeController extends Controller
      */
     public function welcome()
     {
-       return view('welcome');
+      $carts = Cart::all();
+      $stores = Store::all()->orderBy('name');
+      $cities = City::all()->orderBy('name');
+      $issues = Issue::all()->orderBy('name');
+      $maintenances = Maintenance::all()->orderBy('name');
+      $products = Product::all()->orderBy('name')->with('category');
+      $regions = Region::all()->orderBy('name');
+      $users = User::all()->orderBy('username');
+      $states = State::all()->orderBy('id');
+
+      return view('welcome', compact(
+        'stores', 'cities', 'issues', 'maintenances', 'products',
+        'regions', 'users', 'states'
+      ));
     }
 
     public function index()
