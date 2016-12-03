9cc740fa687784a863baa3ac66a208849a74afb3
codeapps <codeapps@aol.com>
2016-12-03 16:57:23 -0500

    Metrics and PHPDocumentor

--- database/seeds/CategoriesTableSeeder.php
+++ database/seeds/CategoriesTableSeeder.php
@@ -11,7 +11,15 @@ class CategoriesTableSeeder extends Seeder
      */
     public function run()
     {
-      factory(App\Category::class, 10)->create();
+      factory(App\Category::class)->create(['name' => 'Notebooks']);
+      factory(App\Category::class)->create(['name' => 'Tablets']);
+      factory(App\Category::class)->create(['name' => 'Ratones']);
+      factory(App\Category::class)->create(['name' => 'Desktops']);
+      factory(App\Category::class)->create(['name' => 'Herramientas']);
+      factory(App\Category::class)->create(['name' => 'Smartphones']);
+      factory(App\Category::class)->create(['name' => 'Antenas']);
+      factory(App\Category::class)->create(['name' => 'Teclados']);
+      factory(App\Category::class)->create(['name' => 'Audifonos']);
 
       echo "Done" . PHP_EOL;
     }
