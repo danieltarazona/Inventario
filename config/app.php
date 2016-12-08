8cdb5654fefcf5027a0c0215db694b2c740c5a18
codeapps <codeapps@aol.com>
2016-12-07 22:21:17 -0500

    Laravel 5 PDF and Excel Support

--- config/app.php
+++ config/app.php
@@ -169,6 +169,8 @@ return [
          */
         Intervention\Image\ImageServiceProvider::class,
         Collective\Html\HtmlServiceProvider::class,
+        Maatwebsite\Excel\ExcelServiceProvider::class,
+        Barryvdh\DomPDF\ServiceProvider::class,
         //
 
         /*
@@ -230,6 +232,8 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Image' => Intervention\Image\Facades\Image::class,
+        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'PDF' => Barryvdh\DomPDF\Facade::class,
         'Carbon' => 'Carbon\Carbon',
 
 
